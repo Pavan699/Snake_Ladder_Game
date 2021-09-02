cad97944eb95c71cdf5cd78c9e96b62abb2df18d
Pavan699 <pavannakate03@gmail.com>
2021-09-02 01:00:37 -0700

    Snake_Lasdder with UC1 and UC2

---
+++ Snake_Ladder_Game/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Snake_Ladder_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake_Ladder_Game");
+            Random r = new Random();
+            int dice = r.Next(1, 6 + 1);
+            int position_one = 0;
+            Console.WriteLine("Position : "+position_one);
+            Console.WriteLine("Dice Num is : "+dice);
+        }
+    }
+}
