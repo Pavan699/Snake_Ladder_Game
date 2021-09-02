71a303c2f7e17386b61112a3cb3a2382b5b1e39e
Pavan699 <pavannakate03@gmail.com>
2021-09-02 02:19:38 -0700

    Solved UC3,UC4 and UC5

--- Snake_Ladder_Game/Program.cs
+++ Snake_Ladder_Game/Program.cs
@@ -8,10 +8,93 @@ namespace Snake_Ladder_Game
         {
             Console.WriteLine("Welcome to Snake_Ladder_Game");
             Random r = new Random();
-            int dice = r.Next(1, 6 + 1);
-            int position_one = 0;
-            Console.WriteLine("Position : "+position_one);
-            Console.WriteLine("Dice Num is : "+dice);
+            int position_one = 0,count_one = 1;
+            while (position_one != 100)
+            {
+                int dice = r.Next(1, 6 + 1);
+                Console.WriteLine("Dice Num is : " + dice);
+                position_one = position_one + dice; 
+                Console.WriteLine("Position : " + position_one);
+                count_one = count_one + 1;
+                
+                //if conditions for ladder
+                if (position_one == 4)
+                {
+                    position_one = position_one + dice;
+                    Console.WriteLine("Got Ladder :: "+position_one);
+                }
+                if (position_one == 13)
+                {
+                    position_one = position_one + dice;
+                    Console.WriteLine("Got Ladder :: " + position_one);
+                }
+                if (position_one == 26)
+                {
+                    position_one = position_one + dice;
+                    Console.WriteLine("Got Ladder :: " + position_one);
+                }
+                if (position_one == 34)
+                {
+                    position_one = position_one + dice;
+                    Console.WriteLine("Got Ladder :: " + position_one);
+                }
+                if (position_one == 56)
+                {
+                    position_one = position_one + dice;
+                    Console.WriteLine("Got Ladder :: " + position_one);
+                }
+                if (position_one == 76)
+                {
+                    position_one = position_one + dice;
+                    Console.WriteLine("Got Ladder :: " + position_one);
+                }
+                if (position_one == 89)
+                {
+                    position_one = position_one + dice;
+                    Console.WriteLine("Got Ladder :: " + position_one);
+                }
+
+                //if for snake
+                if (position_one == 9)
+                {
+                    position_one = position_one - dice;
+                    Console.WriteLine("Got Snake :: " + position_one);
+                }
+                if (position_one == 15)
+                {
+                    position_one = position_one - dice;
+                    Console.WriteLine("Got Snake :: " + position_one);
+                }
+                if (position_one == 35)
+                {
+                    position_one = position_one - dice;
+                    Console.WriteLine("Got Snake :: " + position_one);
+                }
+                if (position_one == 59)
+                {
+                    position_one = position_one - dice;
+                    Console.WriteLine("Got Snake :: " + position_one);
+                }
+                if (position_one == 72)
+                {
+                    position_one = position_one - dice;
+                    Console.WriteLine("Got Snake :: " + position_one);
+                }
+                if (position_one == 98)
+                {
+                    position_one = position_one - dice;
+                    Console.WriteLine("Got Snake :: " + position_one);
+                }
+
+                //if for greter than 100
+                if (position_one >100)
+                {
+                    position_one = position_one - dice;
+                    Console.WriteLine("Greter than 100");
+                    Console.WriteLine("Reduce dice :: "+position_one);
+                }
+            }
+            Console.WriteLine("Counter is : " + count_one);
         }
     }
 }
